a1122d551b1b4c101aaea9a1db5a621d054eb2e5
onkarsingh <onkarsingh@gofynd.com>
2025-04-04 17:59:12 +0530

    add dummy response

--- server/main.go
+++ server/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/panikdkernel/github-search-api-grpc/githubsearch_proto"
+	"google.golang.org/grpc"
 )
 
+type server struct {
+	pb.UnimplementedGithubSearchServiceServer
+}
+
+func (s *server) Search(ctx context.Context, req *pb.SearchRequest) (*pb.SearchResponse, error) {
+	// Dummy implementation
+	log.Printf("Search called with term: %s, user: %s", req.SearchTerm, req.User)
+
+	// Sample response
+	results := []*pb.Result{
+		{FileUrl: "https://github.com/example/file1", Repo: "example/repo1"},
+		{FileUrl: "https://github.com/example/file2", Repo: "example/repo2"},
+	}
+	return &pb.SearchResponse{Results: results}, nil
+}
+
 func main() {
-	fmt.Println("Init Server")
+	lis, err := net.Listen("tcp", ":9001")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGithubSearchServiceServer(s, &server{})
+
+	log.Println("gRPC server running on :9001")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
